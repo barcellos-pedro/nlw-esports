c0e9e7f3b0e5aded74d0abab521bfd26ee22b7b1
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-11 23:16:58 -0300

    Criar projeto Mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+
+      <Text style={styles.text}>Hello world!</Text>
+
+      <TouchableOpacity>
+        <Text>Click to start! 🎯</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+  },
+});
