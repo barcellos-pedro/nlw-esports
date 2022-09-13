cfb491b34b8291349d6b04dd94d496aa4fab63d3
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-13 14:22:05 -0300

    Add image, heading and styles

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,10 +1,16 @@
-import { View } from 'react-native';
+import { Image, View } from 'react-native';
 import { styles } from './styles';
+import logo from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
 
 export function Home() {
   return (
     <View style={styles.container}>
-      
+      <Image source={logo} style={styles.logo} />
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
     </View>
   );
 }
--- mobile/src/screens/Home/styles.ts
+++ mobile/src/screens/Home/styles.ts
@@ -1,5 +1,13 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  logo: {
+    maxWidth: '100%',
+    marginTop: 74,
+    marginBottom: 48,
+  },
 });
