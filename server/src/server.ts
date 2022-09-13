33659f52d6182e720c9a5e33ae248dfdc194293b
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-13 20:50:47 -0300

    Add routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,24 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json({
-    id: 1,
-    message: 'hello world!',
-    status: true
-  });
+// List games with ads count
+app.get('/games', (request, response) => {
+  return response.json({});
+});
+
+// Create new ad
+app.post('/ads', (request, response) => {
+  return response.json([]);
+});
+
+// List ads by game
+app.get('/games/:id/ads', (request, response) => {
+  return response.send(request.params.id);
+});
+
+// Find Discord by Ad ID
+app.get('/ads/:id/discord', (request, response) => {
+  return response.send(request.params.id);
 });
 
 app.listen(3000, () => console.log('Server running on localhost:3000'));
