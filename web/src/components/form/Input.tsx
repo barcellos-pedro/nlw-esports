873f75e7e98cf3d624e9dd91322d16d20dcb8d51
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-15 17:44:26 -0300

    Remove styles

--- web/src/components/form/Input.tsx
+++ web/src/components/form/Input.tsx
@@ -6,7 +6,7 @@ export function Input({ ...htmlAttrs }: InputProps) {
   return (
     <input
       {...htmlAttrs}
-      className="block w-full rounded py-3 px-4 mt-2 bg-zinc-900 text-sm placeholder:text-zinc-500"
+      className="rounded py-3 px-4 bg-zinc-900 text-sm placeholder:text-zinc-500"
     />
   );
 }
