ecfbdc746fcebf33f5cbda12725c3979fd7628c3
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-16 10:56:17 -0300

    Remove style

--- web/src/components/CreateAdBanner.tsx
+++ web/src/components/CreateAdBanner.tsx
@@ -7,7 +7,7 @@ export function CreateAdBanner({ children }: CreateAdBannerPros) {
     <div className="p-1 bg-nlw-gradient self-stretch rounded-lg mt-8">
       <div className="bg-[#2A2634] px-8 py-6 grid grid-col-1 gap-4 md:flex justify-between items-center">
         <div>
-          <strong className="text-2xl text-white font-black block">
+          <strong className="text-2xl text-white font-black">
             Não encontrou seu duo?
           </strong>
           <span className="text-zinc-400 block">
