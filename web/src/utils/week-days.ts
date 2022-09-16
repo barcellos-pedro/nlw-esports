5b977e480f5b9f490ba1680b7fce9123bf3626ff
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-15 21:59:09 -0300

    Create utils

---
+++ web/src/utils/week-days.ts
@@ -0,0 +1,11 @@
+export const WEEKDAYS = [
+  'Doming',
+  'Segunda',
+  'Terça',
+  'Quarta',
+  'Quinta',
+  'Sexta',
+  'Sábado',
+];
+
+export const getFirstLetter = (value: string) => value[0].toUpperCase();
