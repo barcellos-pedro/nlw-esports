4f7257ecc0464f5154cd84331582a62c1ee0dbdd
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-19 20:19:11 -0300

    Add get Ads

--- mobile/src/utils/games-service.ts
+++ mobile/src/utils/games-service.ts
@@ -1,9 +1,10 @@
+import { Ad } from '../@types/ad';
 import { Game } from '../@types/game';
 import { handleResponse } from './handle-response';
 import { sleep } from './sleep';
 
 // Get IP from WSL 2 distro => wsl hostname -I
-const IP = '172.26.247.74';
+const IP = '172.23.159.244';
 const PORT = '3000';
 const BASE_URL = `http://${IP}:${PORT}`;
 
@@ -18,5 +19,18 @@ export const gamesService = {
       .catch((error) => {
         throw error;
       });
+  },
+  async getAds(id: string) {
+    try {
+      const response = await fetch(`${BASE_URL}/games/${id}/ads`);
+      const data = await handleResponse<Ad[]>(
+        response,
+        'Failed to fetch game Ads'
+      );
+      await sleep();
+      return data;
+    } catch (error) {
+      throw error;
+    }
   },
 };
