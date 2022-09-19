5c258fcf11d81f762133642f7467982a17a8eb6c
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-19 09:49:59 -0300

    Use Background on screen

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -9,6 +9,7 @@ import { Loading } from '../../components/Loading';
 import { Game } from '../../@types/game';
 import { gamesService } from '../../utils/games-service';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Background } from '../../components/Background';
 
 export function Home() {
   const [games, setGames] = useState<Game[]>([]);
@@ -29,21 +30,25 @@ export function Home() {
   }
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Image source={logo} style={styles.logo} />
-      <Heading
-        style={styles.heading}
-        title="Encontre seu duo!"
-        subtitle="Selecione o game que deseja jogar..."
-      />
-      <FlatList
-        data={games}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.contentList}
-        keyExtractor={(game) => game.id}
-        renderItem={({ item: game }) => <GameCard game={game} key={game.id} />}
-      />
-    </SafeAreaView>
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image source={logo} style={styles.logo} />
+        <Heading
+          style={styles.heading}
+          title="Encontre seu duo!"
+          subtitle="Selecione o game que deseja jogar..."
+        />
+        <FlatList
+          data={games}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.contentList}
+          keyExtractor={(game) => game.id}
+          renderItem={({ item: game }) => (
+            <GameCard game={game} key={game.id} />
+          )}
+        />
+      </SafeAreaView>
+    </Background>
   );
 }
