20760f6ef77c680564cd78146be441655b097b30
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-19 16:03:55 -0300

    Implement GameScreen

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,12 +1,43 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Image, TouchableOpacity, View } from 'react-native';
 import { Background } from '../../components/Background';
-import { styles } from './styles';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { Entypo } from '@expo/vector-icons';
 
-export function Game() {
+import logo from '../../assets/logo-nlw-esports.png';
+import { styles } from './styles';
+import { Game } from '../../@types/game';
+import { THEME } from '../../theme';
+import { Heading } from '../../components/Heading';
+
+export function GameScreen() {
+  const route = useRoute();
+  const game = route.params as Game;
+  const navigation = useNavigation();
+  const goBack = () => navigation.goBack();
+
   return (
     <Background>
       <SafeAreaView style={styles.container}>
-        
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Entypo
+              name="chevron-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={32}
+              onPress={goBack}
+            />
+          </TouchableOpacity>
+          <Image source={logo} style={styles.logo} />
+          <View style={styles.right} />
+        </View>
+
+        <Image
+          source={{ uri: game.bannerUrl }}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+        <Heading subtitle="Conect-se e comece a jogar!" title={game.title} />
       </SafeAreaView>
     </Background>
   );
--- mobile/src/screens/Game/styles.ts
+++ mobile/src/screens/Game/styles.ts
@@ -1,5 +1,31 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1, // fills whole screen and flex column by default
+    alignItems: 'center',
+  },
+  header: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 32,
+    marginTop: 28,
+  },
+  logo: {
+    width: 71,
+    height: 40,
+  },
+  right: {
+    width: 20,
+    height: 20,
+  },
+  cover: {
+    width: 311,
+    height: 160,
+    borderRadius: 8,
+    marginTop: 32,
+    marginBottom: 24,
+  },
 });
