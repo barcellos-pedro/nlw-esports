05c230b26b7328d24728a7a9745d41c36a71a656
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-19 20:18:35 -0300

    Create type

---
+++ mobile/src/@types/ad.ts
@@ -0,0 +1,10 @@
+export interface Ad {
+  id: string;
+  name: string;
+  yearsPlaying: number;
+  weekDays: string[];
+  hourStart: string;
+  hourEnd: string;
+  useVoiceChannel: boolean;
+  createdAt: string;
+}
