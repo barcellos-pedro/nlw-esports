4ed55f4fb2f55b66d0a7f60786a20457bfe88b99
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-19 22:00:52 -0300

    Add prop

--- mobile/src/components/DuoInfo/index.tsx
+++ mobile/src/components/DuoInfo/index.tsx
@@ -1,16 +1,23 @@
-import { View, Text, ViewProps } from 'react-native';
+import { View, Text, ViewProps, ColorValue } from 'react-native';
+import { THEME } from '../../theme';
 import { styles } from './styles';
 
 interface DuoInfoProps extends ViewProps {
   label: string;
   value: string;
+  valueColor?: ColorValue;
 }
 
-export function DuoInfo({ label, value, ...viewProps }: DuoInfoProps) {
+export function DuoInfo({
+  label,
+  value,
+  valueColor = THEME.COLORS.TEXT,
+  ...viewProps
+}: DuoInfoProps) {
   return (
     <View style={styles.container} {...viewProps}>
       <Text style={styles.label}>{label}</Text>
-      <Text style={styles.value}>{value}</Text>
+      <Text style={[styles.value, { color: valueColor }]}>{value}</Text>
     </View>
   );
 }
