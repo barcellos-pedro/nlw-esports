1f1566dd158ec28f131093e1f75014d634fe19cf
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-19 22:09:50 -0300

    Implement DuoCard

--- mobile/src/components/DuoCard/index.tsx
+++ mobile/src/components/DuoCard/index.tsx
@@ -4,16 +4,47 @@ import { Entypo } from '@expo/vector-icons';
 import { DuoInfo } from '../DuoInfo';
 import { styles } from './styles';
 import { THEME } from '../../theme';
+import { Ad } from '../../@types/ad';
 
-interface DuoCardProps extends ViewProps {}
+interface DuoCardProps extends ViewProps {
+  info: Ad;
+}
 
-export function DuoCard({ ...viewProps }: DuoCardProps) {
+export function DuoCard({ info, ...viewProps }: DuoCardProps) {
+  const getHour = (hour: 'hourStart' | 'hourEnd') => info[hour].split(':')[0];
+  const getWeekDays = () => {
+    const daysCount = info.weekDays.length;
+    const start = getHour('hourStart');
+    const end = getHour('hourEnd');
+    return `${daysCount} dia(s) - ${start}h - ${end}h`;
+  };
+
+  const useVoiceChannel = () => info.useVoiceChannel;
+
+  const voiceChannelTextColor = () => {
+    const { ALERT, SUCCESS } = THEME.COLORS;
+    return useVoiceChannel() ? SUCCESS : ALERT;
+  };
+
   return (
     <View style={styles.container} {...viewProps}>
-      <DuoInfo style={styles.info} label="Nome" value="Diego Fernandes" />
-      <DuoInfo style={styles.info} label="Tempo de jogo" value="2 anos" />
-      <DuoInfo style={styles.info} label="Disponibilidade" value="3 dias - 18h - 20h" />
-      <DuoInfo style={styles.info} label="Chamada de áudio?" value="Sim" />
+      <DuoInfo label="Nome" style={styles.info} value={info.name} />
+      <DuoInfo
+        label="Tempo de jogo"
+        style={styles.info}
+        value={`${String(info.yearsPlaying)} ano(s)`}
+      />
+      <DuoInfo
+        label="Disponibilidade"
+        style={styles.info}
+        value={getWeekDays()}
+      />
+      <DuoInfo
+        label="Chamada de áudio?"
+        style={styles.info}
+        value={useVoiceChannel() ? 'Sim' : 'Não'}
+        valueColor={voiceChannelTextColor()}
+      />
       <TouchableOpacity style={styles.button}>
         <Entypo name="game-controller" color={THEME.COLORS.TEXT} size={20} />
         <Text style={styles.buttonText}>Conectar</Text>
