4be06a1385cd175464fe7a090ee82c82ec1e7162
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-19 22:32:09 -0300

    Implement FlatList

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,5 +1,5 @@
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Image, TouchableOpacity, View } from 'react-native';
+import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
 import { Background } from '../../components/Background';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { Entypo } from '@expo/vector-icons';
@@ -9,12 +9,31 @@ import { styles } from './styles';
 import { Game } from '../../@types/game';
 import { THEME } from '../../theme';
 import { Heading } from '../../components/Heading';
+import { DuoCard } from '../../components/DuoCard';
+import { useEffect, useState } from 'react';
+import { Ad } from '../../@types/ad';
+import { gamesService } from '../../utils/games-service';
 
 export function GameScreen() {
   const route = useRoute();
   const game = route.params as Game;
   const navigation = useNavigation();
+  const [ads, setAds] = useState<Ad[]>([]);
+
   const goBack = () => navigation.goBack();
+
+  const fetchAds = async () => {
+    try {
+      const data = await gamesService.getAds(game.id);
+      setAds(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAds();
+  }, []);
 
   return (
     <Background>
@@ -37,7 +56,23 @@ export function GameScreen() {
           style={styles.cover}
           resizeMode="cover"
         />
-        <Heading subtitle="Conect-se e comece a jogar!" title={game.title} />
+        <Heading
+          subtitle="Conecte-se e comece a jogar!"
+          title={game.title}
+          style={styles.heading}
+        />
+
+        {!ads.length ? (
+          <Text style={styles.alert}>Sem anúncios</Text>
+        ) : (
+          <FlatList
+            data={ads}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.contentList}
+            renderItem={({ item: ad }) => <DuoCard info={ad} key={ad.id} />}
+          />
+        )}
       </SafeAreaView>
     </Background>
   );
