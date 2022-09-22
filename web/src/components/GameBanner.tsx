cf3af74d84bbc48e199e3d04ea892293acc9a269
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-22 09:31:15 -0300

    Add style and alt

--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -6,8 +6,8 @@ interface GameBannerProps {
 
 export function GameBanner({ title, bannerUrl, adsCount }: GameBannerProps) {
   return (
-    <a href="" className="relative rounded-lg overflow-hidden">
-      <img src={bannerUrl} alt="" />
+    <a href="" className="relative rounded-lg overflow-hidden md:hover:-translate-y-4 duration-300">
+      <img src={bannerUrl} alt={`${title} game image`} />
       <div className="bg-game-gradient absolute w-full pt-16 pb-4 px-4 bottom-0 left-0 right-0">
         <strong className="font-bold text-white block">{title}</strong>
         <span className="text-zinc-300 text-sm block">
