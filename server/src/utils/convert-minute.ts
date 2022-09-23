61ab058227150be8d0cf7b6e2f0a5694172f62b6
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-23 17:18:09 -0300

    Add comments

--- server/src/utils/convert-minute.ts
+++ server/src/utils/convert-minute.ts
@@ -7,8 +7,8 @@
 export function convertMinutesToHour(minutesAmount: number): string {
   const hours = Math.floor(minutesAmount / 60);
   const minutes = minutesAmount % 60;
-  const formattedHours = String(hours).padStart(2, '0'); // 5:00 => 05:00
-  const formattedString = String(minutes).padStart(2, '0'); // 12:3 => 12:35
+  const formattedHours = String(hours).padStart(2, '0'); // 5 => 05
+  const formattedString = String(minutes).padStart(2, '0'); // 3 => 05:03
 
   return `${formattedHours}:${formattedString}`;
 }
