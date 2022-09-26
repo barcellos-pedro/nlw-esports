c412beb90fbdb5437bd3ba8c9c65dbf7dec38542
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-26 12:14:48 -0300

    Remove onConnect prop and use modal component

--- mobile/src/components/DuoCard/index.tsx
+++ mobile/src/components/DuoCard/index.tsx
@@ -5,14 +5,18 @@ import { DuoInfo } from '../DuoInfo';
 import { styles } from './styles';
 import { THEME } from '../../theme';
 import { Ad } from '../../@types/ad';
+import { useState } from 'react';
+import { GameModal } from '../GameModal';
 
 interface DuoCardProps extends ViewProps {
   info: Ad;
-  onConnect: () => void;
 }
 
-export function DuoCard({ info, onConnect, ...viewProps }: DuoCardProps) {
+export function DuoCard({ info, ...viewProps }: DuoCardProps) {
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+
   const getHour = (hour: 'hourStart' | 'hourEnd') => info[hour].split(':')[0];
+
   const getWeekDays = () => {
     const daysCount = info.weekDays.length;
     const start = getHour('hourStart');
@@ -46,10 +50,23 @@ export function DuoCard({ info, onConnect, ...viewProps }: DuoCardProps) {
         value={useVoiceChannel() ? 'Sim' : 'Não'}
         valueColor={voiceChannelTextColor()}
       />
-      <TouchableOpacity style={styles.button} onPress={onConnect}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setModalVisible(true)}
+      >
         <Entypo name="game-controller" color={THEME.COLORS.TEXT} size={20} />
         <Text style={styles.buttonText}>Conectar</Text>
       </TouchableOpacity>
+
+      <GameModal
+        transparent
+        animationType="slide"
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+        title="Let's play!"
+        description="Agora é só começar a jogar!"
+        buttonDescription="Adicione no Discord"
+      />
     </View>
   );
 }
