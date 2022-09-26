993e26c3e1c10c80db6042ba2d7b2544871b94d7
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-09-26 12:13:19 -0300

    Create GameModal Component

---
+++ mobile/src/components/GameModal/index.tsx
@@ -0,0 +1,64 @@
+import {
+  Modal,
+  ModalProps,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+import { style } from './styles';
+import { THEME } from '../../theme';
+
+interface GameModalProps extends ModalProps {
+  title: string;
+  description: string;
+  buttonDescription: string;
+  onClose: () => void;
+}
+
+export function GameModal({
+  title,
+  description,
+  buttonDescription,
+  onClose,
+  ...modalProps
+}: GameModalProps) {
+  return (
+    <Modal {...modalProps}>
+      {/* Overlay */}
+      <View style={style.overlayContainer}>
+        {/* Content */}
+        <View style={style.contentContainer}>
+          <Pressable onPress={onClose} style={style.closeButton}>
+            <AntDesign
+              name="close"
+              size={24}
+              color={THEME.COLORS.CAPTION_500}
+            />
+          </Pressable>
+
+          <View style={style.modalBody}>
+            <FontAwesome5
+              name="discord"
+              color={THEME.COLORS.SUCCESS}
+              size={64}
+            />
+            <Text style={style.title}>{title}</Text>
+            <Text style={style.description}>{description}</Text>
+          </View>
+
+          <View style={style.modalFooter}>
+            <Text style={style.buttonDescription}>{buttonDescription}</Text>
+
+            <TouchableOpacity style={style.button}>
+              <Text style={style.buttonText}>Discord#1234</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+}
---
+++ mobile/src/components/GameModal/styles.ts
@@ -0,0 +1,57 @@
+import { StyleSheet } from 'react-native';
+import { THEME } from '../../theme';
+
+export const style = StyleSheet.create({
+  overlayContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(18, 18, 20, 0.7)',
+  },
+  contentContainer: {
+    width: 311,
+    backgroundColor: THEME.COLORS.SHAPE,
+    paddingVertical: 32,
+    paddingHorizontal: 40,
+    borderRadius: 8,
+  },
+  closeButton: {
+    alignSelf: 'flex-end',
+  },
+  modalBody: {
+    alignItems: 'center',
+  },
+  title: {
+    marginTop: 24,
+    color: THEME.COLORS.TEXT,
+    fontFamily: THEME.FONT_FAMILY.BLACK,
+    fontSize: THEME.FONT_SIZE.LG,
+  },
+  description: {
+    color: THEME.COLORS.TEXT,
+    fontFamily: THEME.FONT_FAMILY.REGULAR,
+    fontSize: THEME.FONT_SIZE.MD,
+  },
+  modalFooter: {
+    marginTop: 24,
+    alignItems: 'center',
+  },
+  buttonDescription: {
+    color: THEME.COLORS.TEXT,
+    fontFamily: THEME.FONT_FAMILY.SEMI_BOLD,
+    fontSize: THEME.FONT_SIZE.MD,
+  },
+  button: {
+    backgroundColor: THEME.COLORS.BACKGROUND_900,
+    borderRadius: 4,
+    alignItems: 'center',
+    paddingVertical: 11,
+    width: 231,
+    marginTop: 8,
+  },
+  buttonText: {
+    color: THEME.COLORS.CAPTION_300,
+    fontFamily: THEME.FONT_FAMILY.REGULAR,
+    fontSize: THEME.FONT_SIZE.MD,
+  },
+});
